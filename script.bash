0d6520f047d03218f58c230a6450f25772bcec14
jumah.mo <jumah.mohammad@gmail.com>
2021-09-12 22:35:44 +0100

    updating html and adding wait time to bash script

--- script.bash
+++ script.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 aws cloudformation create-stack --stack-name mystack --template-body file://s3.yaml
-# add wait time 
+# add wait time
+sleep 3 
 aws s3 cp cake.html s3://cake.halab.info
 aws s3 cp error.html s3://cake.halab.info
